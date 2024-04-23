145a791bf65183f0948217780d70e3e5c5387e51
codingleaf <155798178+codingleaf@users.noreply.github.com>
2024-04-23 19:30:45 +0800

    feat: add jwt and error prompts in signup

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,4 +1,5 @@
 import User from '../models/user-model.js';
+import jwt from 'jsonwebtoken';
 
 const handleErrors = (err) => {
   console.log(err.message, err.code);
@@ -20,19 +21,27 @@ const handleErrors = (err) => {
   return errors;
 }
 
+const maxAge = 60 * 60 * 24 // 1 day
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, {
+    expiresIn: maxAge
+  })
+}
+
 export const signup_get = (req, res) => {
   res.render('signup', { title: 'SIGN UP' });
 };
 
 export const signup_post = async (req, res) => {
   const { firstName, lastName, email, password, role } = req.body;
-  
   try {
     const user = await User.create({ firstName, lastName, email, password, role });
-    res.status(201).json(user);
+    const token = createToken(user._id); // set json web token
+    res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000}); // set cookie
+    res.status(201).json( { user: user._id } );
   } catch (err) {
     const errors = handleErrors(err);
-    res.status(400).json(errors);
+    res.status(400).json({ errors });
   }
 };
 
